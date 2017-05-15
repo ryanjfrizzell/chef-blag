a7ac95efe3c4c45dca3ddb247c74fc73607bd7b1
Ryan Frizzell <ryan.frizzell@sendgrid.com>
2017-05-14 18:37:12 -0600

    initial commit

--- metadata.rb
+++ metadata.rb
@@ -1,19 +1,11 @@
 name 'blag'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'Ryan Frizzell'
+maintainer_email 'no@bugmenot.com'
 license 'all_rights'
 description 'Installs/Configures blag'
 long_description 'Installs/Configures blag'
 version '0.1.0'
+issues_url 'https://github.com/<insert_org_here>/blag/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/<insert_org_here>/blag' if respond_to?(:source_url)
 
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/blag/issues' if respond_to?(:issues_url)
-
-# The `source_url` points to the development reposiory for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/blag' if respond_to?(:source_url)
+cookbook 'nginx', '~> 2.7.6'
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,3 @@
-#
 # Cookbook:: blag
 # Recipe:: default
-#
-# Copyright:: 2017, The Authors, All Rights Reserved.
+
